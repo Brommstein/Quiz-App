59ebd25b304356a4a3172e521096c808c2a1e00c
Ryan Conley <ryandeanconley2@yahoo.com>
2020-11-24 06:51:12 -0600

    completion

--- app.js
+++ app.js
@@ -5,83 +5,18 @@
  * Example store structure
  */
 
-// Characters that will be fighting... Ashe, Garen, Ziggs, Alistar, Fiora, Zed
-
-const characters = {
-  char: [
-    {
-      name: 'Ashe',
-      type: 'Marksman',
-      image: '<img src="images/poroAshe.jpg" alt="Ashe">',
-      pro: 'Likes long fights at a range.',
-      con: 'Loses to high damage.',
-      loss: ['Zed', 'Ziggs'],
-      tie: 'Darius',
-      wins: ['Braum', 'Garen']
-    },
-    {
-      name: 'Braum',
-      type: 'Support',
-      image: '<img src="images/poroBraum.jpg" alt="Braum">',
-      pro: 'Good defense and good sustain.',
-      con: 'Loses to long range.',
-      loss: ['Ashe', 'Garen'],
-      tie: 'Zed',
-      wins: ['Ziggs', 'Darius']
-    },
-    {
-      name: 'Darius',
-      type: 'Fighter',
-      image: '<img src="images/poroDarius.jpg" alt="Darius">',
-      pro: 'Likes long fights.',
-      con: 'Loses to good sustain.',
-      loss: ['Braum', 'Ziggs'],
-      tie: 'Ashe',
-      wins: ['Garen', 'Zed']
-    },
-    {
-      name: 'Garen',
-      type: 'Tank',
-      image: '<img src="images/poroGaren.jpg" alt="Garen">',
-      pro: 'High defense.',
-      con: 'Loses to long range.',
-      loss: ['Ashe', 'Darius'],
-      tie: 'Ziggs',
-      wins: ['Braum', 'Zed']
-    },
-    {
-      name: 'Zed',
-      type: 'Assassin',
-      image: '<img src="images/poroZed.png" alt="Zed">',
-      pro: 'High damage and high mobility.',
-      con: 'Loses to long fights.',
-      loss: ['Garen', 'Darius'],
-      tie: 'Braum',
-      wins: ['Ashe', 'Ziggs']
-    },
-    {
-      name: 'Ziggs',
-      type: 'Mage',
-      image: '<img src="images/poroZiggs.png" alt="Ziggs">',
-      pro: 'Good damage from long distance.',
-      con: 'Loses to high damage.',
-      loss: ['Zed', 'Braum'],
-      tie: 'Garen',
-      wins: ['Ashe', 'Darius']
-    },
-  ]
-};
+// Characters that will be fighting... Ashe, Garen, Ziggs, Braum, Darius, Zed
 
 function fightResults(one, two){
-  if (characters.char[one].tie === characters.char[two].name || characters.char[one].name === characters.char[two].name){
+  if (store.characters.char[one].tie === store.characters.char[two].name || store.characters.char[one].name === store.characters.char[two].name){
     return 'Tie';
   } else {
     for (let i = 0; i < 2; i++){
-      if (characters.char[one].loss[i] === characters.char[two].name){
-        return characters.char[two].name;
+      if (store.characters.char[one].loss[i] === store.characters.char[two].name){
+        return store.characters.char[two].name;
       }
-      if (characters.char[one].wins[i] === characters.char[two].name){
-        return characters.char[one].name;
+      if (store.characters.char[one].wins[i] === store.characters.char[two].name){
+        return store.characters.char[one].name;
       }
     }
   }
@@ -92,10 +27,10 @@ function start() {
   for (let i = 0; i < 10; i++) {
     let numOne =Math.floor(Math.random()*5);
     let numTwo = Math.floor(Math.random()*5);
-    let randVsOne = characters.char[numOne].name;
-    let randVsTwo = characters.char[numTwo].name;
+    let randVsOne = store.characters.char[numOne].name;
+    let randVsTwo = store.characters.char[numTwo].name;
     if (randVsTwo === randVsOne) {
-      randVsTwo = characters.char[numTwo].name;
+      randVsTwo = store.characters.char[numTwo].name;
     }
     store.questions.push({
       question: `Who will win between ${randVsOne} and ${randVsTwo}?`,
@@ -110,6 +45,70 @@ function start() {
 }
 
 const store = {
+  characters: {
+    char: [
+      {
+        name: 'Ashe',
+        type: 'Marksman',
+        image: '<img src="images/poroAshe.jpg" alt="Ashe">',
+        pro: 'Likes long fights at a range.',
+        con: 'Loses to high damage.',
+        loss: ['Zed', 'Ziggs'],
+        tie: 'Darius',
+        wins: ['Braum', 'Garen']
+      },
+      {
+        name: 'Braum',
+        type: 'Support',
+        image: '<img src="images/poroBraum.jpg" alt="Braum">',
+        pro: 'Good defense and good sustain.',
+        con: 'Loses to long range.',
+        loss: ['Ashe', 'Garen'],
+        tie: 'Zed',
+        wins: ['Ziggs', 'Darius']
+      },
+      {
+        name: 'Darius',
+        type: 'Fighter',
+        image: '<img src="images/poroDarius.jpg" alt="Darius">',
+        pro: 'Likes long fights.',
+        con: 'Loses to good sustain.',
+        loss: ['Braum', 'Ziggs'],
+        tie: 'Ashe',
+        wins: ['Garen', 'Zed']
+      },
+      {
+        name: 'Garen',
+        type: 'Tank',
+        image: '<img src="images/poroGaren.jpg" alt="Garen">',
+        pro: 'High defense.',
+        con: 'Loses to long range.',
+        loss: ['Ashe', 'Darius'],
+        tie: 'Ziggs',
+        wins: ['Braum', 'Zed']
+      },
+      {
+        name: 'Zed',
+        type: 'Assassin',
+        image: '<img src="images/poroZed.png" alt="Zed">',
+        pro: 'High damage and high mobility.',
+        con: 'Loses to long fights.',
+        loss: ['Garen', 'Darius'],
+        tie: 'Braum',
+        wins: ['Ashe', 'Ziggs']
+      },
+      {
+        name: 'Ziggs',
+        type: 'Mage',
+        image: '<img src="images/poroZiggs.png" alt="Ziggs">',
+        pro: 'Good damage from long distance.',
+        con: 'Loses to high damage.',
+        loss: ['Zed', 'Braum'],
+        tie: 'Garen',
+        wins: ['Ashe', 'Darius']
+      },
+    ]
+  },
   // 5 or more questions are required
   questions: [],
   quizStarted: false,
@@ -139,7 +138,7 @@ const store = {
 
 
 function generateMain() {
-  return $('.mainPage').html(`<form>
+  return `<form>
   <h2>Lets begin!</h2>
   <div class="group">
   <div class="itemOne">
@@ -151,24 +150,35 @@ function generateMain() {
   </div>
   </div>
   </form>
-  `);
+  `;
+}
+
+function getCharsForQuestion(){
+  console.log(store.characters.char);
+  let charOne = store.characters.char.find(function (characters) {
+    return store.questions[store.questionNumber].answers[0] === characters.name;
+  });
+  console.log(charOne);
+  let charTwo = store.characters.char.find(function (characters) {
+    return store.questions[store.questionNumber].answers[1] === characters.name;
+  });
+  //need to return charOne and charTwo...
+  let chars = [charOne, charTwo];
+  return chars;
 }
 
 function generateQuestion() {
-  let charOne = characters.char.find(function (characters) {
-    return store.questions[store.questionNumber].answers[0] === characters.name;
-  });
-  let charTwo = characters.char.find(function (characters) {
-    return store.questions[store.questionNumber].answers[1] === characters.name;
-  });
-  return $('.mainPage').html(`<form class="questions">
-    <h2>${store.questions[store.questionNumber].question}</h2>
+  return `<form class="questions">
+    <h2 id="question">${store.questions[store.questionNumber].question}</h2>
     <div class="group">
     <div class="itemOne">
-    ${charOne.image}
-    <p>${charOne.type}</p>
-    <p>${charOne.pro}</p>
-    <p>${charOne.con}</p>
+    <div class="charOne">
+    <p></p>
+    <div id="charOneImage"></div>
+    <p></p>
+    <p></p>
+    <p></p>
+    </div>
     </div>
     <div class="itemThree">
     <ul>
@@ -179,69 +189,72 @@ function generateQuestion() {
     <button id="results" type="button">Results</button>
     </div>
     <div class="itemTwo">
-    ${charTwo.image}
-    <p>${charTwo.type}</p>
-    <p>${charTwo.pro}</p>
-    <p>${charTwo.con}</p>
+    <div class="charTwo">
+    <p></p>
+    <div id="charTwoImage"></div>
+    <p></p>
+    <p></p>
+    <p></p>
+    </div>
     </div>
     </div>
     <br>
     <br>
     <br>
     <div class="group scoreBoard">
-    <div class="itemOne">
+    <div class="itemOne bottomTag">
     <p>Question ${store.questionNumber + 1} out of ${store.questions.length}</p>
     </div>
     <div class="itemTwo">
     <p>Current score: ${store.score}</p>
     </div></div>
-</form>`);
+</form>`;
 }
 
 function generateFeedback(answer, correct) {
   if (store.questionNumber === store.questions.length - 1) {
     if (answer === correct) {
       store.score++;
-      return $('.mainPage').html(`<div class="results">
+      return `<div class="results">
     <h2>Correct!</h2>
     <p>Good job! You got that one correct!</p>
     <button id="finalResult"type="button">Final Results</button>
-  </div>`);
+  </div>`;
     } else {
-      return $('.mainPage').html(`<div class="results">
+      return `<div class="results">
     <h2>Incorrect...</h2>
     <p>Sorry but that's incorrect...</p>
     <p>The correct answer was ${store.questions[store.questionNumber].correctAnswer}</p>
     <button id="finalResult"type="button">Final Results</button>
-  </div>`);
+  </div>`;
     }
   } else {
     if (answer === correct) {
       store.score++;
-      return $('.mainPage').html(`<div class="results">
+      return `<div class="results">
     <h2>Correct!</h2>
     <p>Good job! You got that one correct!</p>
-    <button id="nextQuestion"type="button">Next Question</button>
-  </div>`);
+    <button id="nextQuestion" type="button">Next Question</button>
+  </div>`;
     } else {
-      return $('.mainPage').html(`<div class="results">
+      return `<div class="results">
       <h2>Incorrect...</h2>
       <p>Sorry but that's incorrect...</p>
       <p>The correct answer was ${store.questions[store.questionNumber].correctAnswer}.</p>
-      <button id="nextQuestion"type="button">Next Question</button>
-</div>`);
+      <button id="nextQuestion" type="button">Next Question</button>
+</div>`;
     }
   }
 }
 
 function generateResults() {
-  return $('.mainPage').html(`<div class="finalResult">
+  return `<div class="finalResult">
   <h2>Final Results</h2>
   <p>You scored a ${store.score} out of ${store.questions.length}!</p>
   <p>Let me know what you thought about my quiz/game!</p>
   <p>Or play again!</p>
   <button id="restart" type="button">Play Again</button>
-</div>`);
+</div>`;
 }
 
 
@@ -255,9 +268,12 @@ function generateResults() {
 function render() {
   $('#header').html(header());
   if (store.quizStarted === false) {
-    generateMain();
+    $('.mainPage').html(generateMain());
   } else {
-    generateQuestion();
+    let chars = getCharsForQuestion();
+    $('.mainPage').html(generateQuestion());
+    generateCharOne(chars);
+    generateCharTwo(chars);
   }
 }
 
@@ -268,7 +284,42 @@ function render() {
 
 // These functions handle events (submit, click, etc)
 
+function generateCharOne(chars){
+  console.log($('.charOne').children());
+  $('.charOne').children().first().text(
+    chars[0].name
+  );
+  $(chars[0].image).appendTo(
+    $('#charOneImage')
+  );
+  $('.charOne').children().eq(2).text(
+    chars[0].type
+  );
+  $('.charOne').children().eq(3).text(
+    chars[0].pro
+  );
+  $('.charOne').children().eq(4).text(
+    chars[0].con
+  );
+}
 
+function generateCharTwo(chars){
+  $('.charTwo').children().first().text(
+    chars[1].name
+  );
+  $(chars[1].image).appendTo(
+    $('#charTwoImage')
+  );
+  $('.charTwo').children().eq(2).text(
+    chars[1].type
+  );
+  $('.charTwo').children().eq(3).text(
+    chars[1].pro
+  );
+  $('.charTwo').children().eq(4).text(
+    chars[1].con
+  );
+}
 
 function submit() {
   $('.mainPage').on('submit', '', function (event) {
@@ -282,20 +333,20 @@ function nextResult() {
   $('.mainPage').on('click', '#results', function () {
     let answer = $('input[name="answers"]:checked').val();
     let correct = store.questions[store.questionNumber].correctAnswer;
-    if (answer !== undefined) { generateFeedback(answer, correct); }
+    if (answer !== undefined) { $('.mainPage').html(generateFeedback(answer, correct)); }
   });
 }
 
 function nextQuestion() {
   $('.mainPage').on('click', '#nextQuestion', function () {
     store.questionNumber++;
-    generateQuestion();
+    render();
   });
 }
 
 function finalResult() {
   $('.mainPage').on('click', '#finalResult', function () {
-    generateResults();
+    $('.mainPage').html(generateResults());
   });
 }
 
